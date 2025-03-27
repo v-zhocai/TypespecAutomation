3932f6a33501442003f8ae19d2126779315e2176
Tiancheng Zhang (WICRESOFT NORTH AMERICA LTD) <v-zhangtianc@microsoft.com>
2025-03-27 19:10:34 +0800

    :sparkles: pipeline

---
+++ src/emit/emitTypespec.test.ts
@@ -0,0 +1,49 @@
+import {
+  contrastResult,
+  installExtension,
+  preContrastResult,
+  start,
+} from "../common/commonSteps"
+import {
+  emitSelectLanguageForOpenapi,
+  emitSelectProject,
+  emitSelectType,
+} from "../common/emiSteps"
+import { sleep, test } from "../common/utils"
+import path from "node:path"
+
+test("EmitTypespec-OpenAPI Document", async ({ launch }) => {
+  const workspacePath = path.resolve(__dirname, "../../EmitTypespecProject")
+  const { page } = await launch({
+    workspacePath,
+  })
+  await installExtension(page)
+  await start(page, {
+    folderName: "EmitTypespecProject",
+    command: "Emit from Typespec",
+  })
+  await emitSelectProject(page, "TextTranslation")
+
+  await page
+    .getByRole("option", { name: "Choose another emitter" })
+    .locator("a")
+    .click()
+
+  await emitSelectType(page, "OpenAPI Document")
+
+  await emitSelectLanguageForOpenapi(page)
+
+  await preContrastResult(
+    page,
+    "OpenAPI3...Succeeded",
+    "Failed to emit project Successful",
+    [10, 3]
+  )
+  await contrastResult(
+    ["openapi.3.0.yaml"],
+    path.resolve(
+      workspacePath,
+      "./Azure.AI.TextTranslation/tsp-output/@typespec/openapi3"
+    )
+  )
+})
