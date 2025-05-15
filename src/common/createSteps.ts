78fb117efcb77e9117f5e3da43f7c62023b8e771
v-zhocai <v-zhocai@microsoft.com>
2025-05-15 15:47:44 +0800

    Add description comparison for Create Cases

--- src/common/createSteps.ts
+++ src/common/createSteps.ts
@@ -23,17 +23,28 @@ async function selectEmitters(page: Page, emitters?: string[]) {
       checks = await Promise.all(
         emittersConfig.map(async (emitter, index) => {
           const nameLocator = page.locator("a").filter({ hasText: emitter.name });
-          const descriptionLocator = page.locator("a").filter({ hasText: emitter.description });
           const nameExists = await nameLocator.count() > 0;
-          const descriptionExists = await descriptionLocator.count() > 0;
 
-          if (!nameExists || !descriptionExists) {
-            throw new Error(`Failed to find the following emitter name and description: "${emitter.name}" and "(${emitter.description})".`);
+          const nameBoxLocator = page.getByRole("checkbox", { name: `${emitter.name}, @`})
+          let nameBoxLocatorText = await nameBoxLocator.textContent()
+          let nameDescription = nameBoxLocatorText?.slice(emitter.name.length)
+          if (nameDescription && nameDescription.endsWith("Azure")) {
+            nameDescription = nameDescription.slice(0, -5)
           }
-          return nameExists && descriptionExists;
+          if (nameDescription && nameDescription.endsWith("Settings")) {
+            nameDescription = nameDescription.slice(0, -8)
+          }
+
+          if (!nameExists) {
+            throw new Error(`Failed to find the following emitter name: "${emitter.name}".`);
+          }
+          if (nameDescription != emitter.description) {
+            throw new Error(`Comparison failed, matched "${nameDescription}", should be "${emitter.description}".`);
+          }
+          return checks.every((result) => result);
         })
       );
-      return checks.every((result) => result);
+      return true;
     },
     ""
   );
@@ -46,6 +57,7 @@ async function selectEmitters(page: Page, emitters?: string[]) {
  * When creating, select template
  * @param page vscode project
  * @param templateName The name of the template that needs to be selected.
+ * @param templateNameDesctiption The description of the template that needs to be selected.
  */
 async function selectTemplate(page: Page, templateName: string, templateNameDesctiption: string) {
   let templateListName
@@ -61,61 +73,103 @@ async function selectTemplate(page: Page, templateName: string, templateNameDesc
   await retry(
     3,
     async () => {
-      templateListDescription = page.getByLabel(templateNameDesctiption, {exact: true }).locator("a")
-      return (await templateListDescription.count() > 0)
+      let templateListBox = page.getByRole("option", { name: templateName}).locator('label')
+      let templateListDescriptionArr = await templateListBox.allTextContents();
+      templateListDescription = templateListDescriptionArr[0].slice(templateName.length)
+      if (templateNameDesctiption == templateListDescription){
+        return true
+      } else {
+        throw new Error(`Comparison failed, matched "${templateListDescription}", should be "${templateNameDesctiption}".`)
+      }
     },
-    `Failed to match the following template description: "${templateNameDesctiption}".`
+    ""
   )
   await templateListName!.first().click()
 }
 
 /**
- * When creating, input project name
+ * When creating, verify the descripton below, then input project name
  * @param page vscode project
  */
 async function inputProjectName(page: Page) {
+  let titleInfoDescription = "Please input the project name (Press 'Enter' to confirm or 'Escape' to cancel)" 
   await retry(
     3,
     async () => {
-      const titleInfo = page.getByText(/Please .*name/).first()
-      const titleInfoDescription = page.getByText('Please input the project name')
-      return (await titleInfo.count() > 0 && await titleInfoDescription.count() > 0)
+      let titleBox = page.locator('div').filter({ hasText: '0 Results0 SelectedPlease' }).nth(2)
+      let titleBoxText = await titleBox.textContent()
+      if (titleBoxText && titleBoxText.startsWith("0 Results0 Selected")) {
+        titleBoxText = titleBoxText.slice(19)
+      }
+      if (titleBoxText && titleBoxText.endsWith("OK")) {
+        titleBoxText = titleBoxText.slice(0, -2)
+      }
+
+      if (titleBoxText === titleInfoDescription){
+        return true
+      } else {
+        throw new Error(`Comparison failed, matched "${titleBoxText}", should be "${titleInfoDescription}".`)
+      }
     },
-    "Failed to find the project name input box."
+    ""
   )
   await screenShot.screenShot("input_project_name.png")
   await page.keyboard.press("Enter")
 }
 
 /**
- * When creating, input service namespace
+ * When creating, verify the description below, then input service namespace
  * @param page vscode project
  */
 async function inputServiceNameSpace(page: Page) {
+  let titleInfoDescription = "Please provide service namespace in Pascal case: (Press 'Enter' to confirm or 'Escape' to cancel)" 
   await retry(
     3,
     async () => {
-      const titleInfoDescription = page.getByText('Please provide service')
-      return (await titleInfoDescription.count() > 0)
+      let titleBox = page.locator('div').filter({ hasText: '0 Results0 SelectedPlease' }).nth(2)
+      let titleBoxText = await titleBox.textContent()
+      if (titleBoxText && titleBoxText.startsWith("0 Results0 Selected")) {
+        titleBoxText = titleBoxText.slice(19)
+      } 
+      if (titleBoxText && titleBoxText.endsWith("OK")) {
+        titleBoxText = titleBoxText.slice(0, -2)
+      }
+      if (titleBoxText === titleInfoDescription){
+        return true
+      } else {
+        throw new Error(`Comparison failed, matched "${titleBoxText}", should be "${titleInfoDescription}".`)
+      }
     },
-    "Failed to find the service namespace input box."
+    ""
   )
   await screenShot.screenShot("input_service_namespace.png")
   await page.keyboard.press("Enter")
 }
 
 /**
- * When creating, input ARM Resource Provider name
+ * When creating, verify the description below, then input ARM Resource Provider name
  * @param page vscode project
  */
 async function inputARMResourceProviderName(page: Page) {
+  let titleInfoDescription = "Please provide ARM Resource Provider Name in Pascal case, excluding the 'Microsoft.' prefix: (Press 'Enter' to confirm or 'Escape' to cancel)" 
   await retry(
     3,
     async () => {
-      const titleInfoDescription = page.getByText('Please provide ARM Resource')
-      return (await titleInfoDescription.count() > 0)
+      let titleBox = page.locator('div').filter({ hasText: '0 Results0 SelectedPlease' }).nth(2)
+      let titleBoxText = await titleBox.textContent()
+      if (titleBoxText && titleBoxText.startsWith("0 Results0 Selected")) {
+        titleBoxText = titleBoxText.slice(19)
+      } 
+      if (titleBoxText && titleBoxText.endsWith("OK")) {
+        titleBoxText = titleBoxText.slice(0, -2)
+      }
+      if (titleBoxText === titleInfoDescription){
+        return true
+      } else {
+        throw new Error(`Comparison failed, matched "${titleBoxText}", should be "${titleInfoDescription}".`)
+      }
     },
-    "Failed to find the ARM resource name input box."
+    ""
   )
   await screenShot.screenShot("input_ARM_Resource_name.png")
   await page.keyboard.press("Enter")
