48c7489880b1262325edb0c28a58bc0a965c8339
v-zhocai <v-zhocai@microsoft.com>
2025-06-10 18:13:58 +0800

    Combine windows and linux in the same repo(Create cases debug)

--- src/common/commonSteps.ts
+++ src/common/commonSteps.ts
@@ -139,23 +139,12 @@ async function selectFolder(file: string = "") {
     await screenShot.screenShot("select_folder.png")
     await keyboard.pressKey(Key.Enter)
   } else {
+    console.log("selectFolder_file", file)
     if (file == "openapi.3.0.yaml" || file == "ImportTypespecProjectEmptyFolder") {
       await keyboard.pressKey(Key.Down)
       await sleep(3)
     }
-    if (file && file.includes("CreateTypespecProject")) {
-      const keySequence = [
-        Key.Down,
-        Key.Enter,
-        Key.Down,
-        Key.Up,
-        Key.Enter,
-      ];
-      for (const key of keySequence) {
-        await keyboard.pressKey(key);
-        await sleep(2);
-      }
-    }
+    await keyboard.type(file)
     await screenShot.screenShot("select_folder.png")
     await keyboard.pressKey(Key.Enter)
     await keyboard.releaseKey(Key.Enter)
