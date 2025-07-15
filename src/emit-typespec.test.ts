2f0ff75805f629f60be78f8621442d1cf09f59f7
v-zhocai <v-zhocai@microsoft.com>
2025-07-15 13:33:51 +0800

    cover emit cases

--- src/emit-typespec.test.ts
+++ src/emit-typespec.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe } from "vitest"
+import { beforeEach, describe } from "vitest";
 import {
   closeVscode,
   contrastResult,
@@ -6,37 +6,35 @@ import {
   preContrastResult,
   startWithRightClick,
   startWithCommandPalette,
-} from "./common/common-steps"
+} from "./common/common-steps";
 import {
   emitSelectLanguage,
   emitSelectLanguageForOpenapi,
   emitSelectType,
   emiChooseEmitter
-} from "./common/emit-steps"
-import { screenShot, sleep, test, tempDir } from "./common/utils"
-import path from "node:path"
-import fs from "node:fs"
+} from "./common/emit-steps";
+import { screenShot, sleep, test, tempDir } from "./common/utils";
+import path from "node:path";
+import fs from "node:fs";
 
 enum EmitProjectTriggerType {
   Command = "Command",
   Click = "Click",
-}
+};
 
 type EmitConfigType = {
-  caseName: string
-  selectType: string
-  selectTypeLanguage: string
-  triggerType: EmitProjectTriggerType
-  expectedResults: string[]
-}
+  caseName: string;
+  selectType: string;
+  selectTypeLanguage: string;
+  triggerType: EmitProjectTriggerType;
+  expectedResults: string[];
+};
 
 const EmitTypespecProjectFolderPath = path.resolve(tempDir, "EmitTypespecProject");
 const EmitTypespecProjectFolderPathStubJs = path.resolve(tempDir, "EmitTypespecProjectStubJs");
 
 const EmitcaseName = "EmitTypespecProject";
-const EmitCasesConfigList: EmitConfigType[] = []
-
-EmitCasesConfigList.push(
+const EmitCasesConfigList: EmitConfigType[] = [
   {   
     caseName: "EmitTypespecProject-ClientCode-Python-CommandPallette",
     selectType: "Client Code",
@@ -44,24 +42,66 @@ EmitCasesConfigList.push(
     triggerType: EmitProjectTriggerType.Command,
     expectedResults: ["http-client-python"],
   },
-)
+  {
+    caseName: "EmitTypespecProject-ClientCode-Java-CommandPallette",
+    selectType: "Client Code",
+    selectTypeLanguage: "Java",
+    triggerType: EmitProjectTriggerType.Command,
+    expectedResults: ["http-client-java"],
+  },
+  {
+    caseName: "EmitTypespecProject-ClientCode-DotNet-RightClick",
+    selectType: "Client Code",
+    selectTypeLanguage: ".NET",
+    triggerType: EmitProjectTriggerType.Click,
+    expectedResults: ["http-client-csharp"],
+  },
+  {
+    caseName: "EmitTypespecProject-ClientCode-Js-RightClick",
+    selectType: "Client Code",
+    selectTypeLanguage: "JavaScript",
+    triggerType: EmitProjectTriggerType.Click,
+    expectedResults: ["http-client-js"],
+  },
+  {
+    caseName: "EmitTypespecProject-Openapi3-CommandPallette",
+    selectType: "OpenAPI Document",
+    selectTypeLanguage: "OpenAPI3",
+    triggerType: EmitProjectTriggerType.Command,
+    expectedResults: ["openapi3"],
+  },
+  {
+    caseName: "EmitTypespecProject-Openapi3-RightClick",
+    selectType: "OpenAPI Document",
+    selectTypeLanguage: "OpenAPI3",
+    triggerType: EmitProjectTriggerType.Click,
+    expectedResults: ["openapi3"],
+  },
+  {
+    caseName: "EmitTypespecProject-ServerStub-DotNet-CommandPallette",
+    selectType: "Server Stub",
+    selectTypeLanguage: ".NET",
+    triggerType: EmitProjectTriggerType.Command,
+    expectedResults: ["http-server-csharp"],
+  },
+]
 
 beforeEach(() => {
-  let dir = path.resolve(EmitTypespecProjectFolderPath, "tsp-output")
+  let dir = path.resolve(EmitTypespecProjectFolderPath, "tsp-output");
   if (fs.existsSync(dir)) {
     for (const file of fs.readdirSync(dir)) {
-      const filePath = path.resolve(dir, file)
-      fs.rmSync(filePath, { recursive: true, force: true })
+      const filePath = path.resolve(dir, file);
+      fs.rmSync(filePath, { recursive: true, force: true });
     }
   }
-  dir = path.resolve(EmitTypespecProjectFolderPathStubJs, "tsp-output")
+  dir = path.resolve(EmitTypespecProjectFolderPathStubJs, "tsp-output");
   if (fs.existsSync(dir)) {
     for (const file of fs.readdirSync(dir)) {
-      const filePath = path.resolve(dir, file)
-      fs.rmSync(filePath, { recursive: true, force: true })
+      const filePath = path.resolve(dir, file);
+      fs.rmSync(filePath, { recursive: true, force: true });
     }
   }
-})
+});
 
 describe.each(EmitCasesConfigList)("EmitTypespecProject", async (item) => {
   const {
@@ -70,7 +110,7 @@ describe.each(EmitCasesConfigList)("EmitTypespecProject", async (item) => {
     selectTypeLanguage,
     triggerType,
     expectedResults,
-  } = item
+  } = item;
   test(caseName, async ({ launch }) => {
     screenShot.setCaseName(caseName);
     const isServerStubJS =
@@ -79,35 +119,35 @@ describe.each(EmitCasesConfigList)("EmitTypespecProject", async (item) => {
 
     const { page, app, extensionDir } = await launch({
       workspacePath,
-    })
+    });
 
-    await installExtensionForCommand(page, extensionDir)
+    await installExtensionForCommand(page, extensionDir);
     if (triggerType === "Command") {
       await startWithCommandPalette(page, "Emit from Typespec");
     } else if (triggerType === "Click") {
       await startWithRightClick(page, "Emit from TypeSpec", "file");
     }
 
-    await sleep(3)
-    await emiChooseEmitter(page)
-    await emitSelectType(page, selectType)
+    await sleep(3);
+    await emiChooseEmitter(page);
+    await emitSelectType(page, selectType);
     if (selectTypeLanguage === "OpenAPI3") {
-      await emitSelectLanguageForOpenapi(page)
+      await emitSelectLanguageForOpenapi(page);
     } else {
-      await emitSelectLanguage(page, selectTypeLanguage, selectType)
+      await emitSelectLanguage(page, selectTypeLanguage, selectType);
     }
 
-    const contrastMessage = selectTypeLanguage + "...Succeeded"
+    const contrastMessage = selectTypeLanguage + "...Succeeded";
     await preContrastResult(
       page,
       contrastMessage,
       "Failed to emit project Successful",
       [20, 3]
-    )
+    );
 
-    await closeVscode(page)
+    await closeVscode(page);
 
-    const resultFilePath = path.resolve(workspacePath, "./tsp-output/@typespec")
-    await contrastResult(page, expectedResults, resultFilePath)
+    const resultFilePath = path.resolve(workspacePath, "./tsp-output/@typespec");
+    await contrastResult(page, expectedResults, resultFilePath);
   })
 })
