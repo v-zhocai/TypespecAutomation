aaba8527e95e7b8c125ca11c2dd8223c064e593e
v-zhocai <v-zhocai@microsoft.com>
2025-07-16 11:24:10 +0800

    return to one case emit

--- src/emit-typespec.test.ts
+++ src/emit-typespec.test.ts
@@ -42,55 +42,6 @@ const EmitCasesConfigList: EmitConfigType[] = [
     triggerType: EmitProjectTriggerType.Command,
     expectedResults: ["http-client-python"],
   },
-  {
-    caseName: "EmitTypespecProject-ClientCode-Java-CommandPallette",
-    selectType: "Client Code",
-    selectTypeLanguage: "Java",
-    triggerType: EmitProjectTriggerType.Command,
-    expectedResults: ["http-client-java"],
-  },
-  {
-    caseName: "EmitTypespecProject-ClientCode-DotNet-RightClick",
-    selectType: "Client Code",
-    selectTypeLanguage: ".NET",
-    triggerType: EmitProjectTriggerType.Click,
-    expectedResults: ["http-client-csharp"],
-  },
-  {
-    caseName: "EmitTypespecProject-ClientCode-Js-RightClick",
-    selectType: "Client Code",
-    selectTypeLanguage: "JavaScript",
-    triggerType: EmitProjectTriggerType.Click,
-    expectedResults: ["http-client-js"],
-  },
-  {
-    caseName: "EmitTypespecProject-Openapi3-CommandPallette",
-    selectType: "OpenAPI Document",
-    selectTypeLanguage: "OpenAPI3",
-    triggerType: EmitProjectTriggerType.Command,
-    expectedResults: ["openapi3"],
-  },
-  {
-    caseName: "EmitTypespecProject-Openapi3-RightClick",
-    selectType: "OpenAPI Document",
-    selectTypeLanguage: "OpenAPI3",
-    triggerType: EmitProjectTriggerType.Click,
-    expectedResults: ["openapi3"],
-  },
-  {
-    caseName: "EmitTypespecProject-ServerStub-DotNet-CommandPallette",
-    selectType: "Server Stub",
-    selectTypeLanguage: ".NET",
-    triggerType: EmitProjectTriggerType.Command,
-    expectedResults: ["http-server-csharp"],
-  },
-  {
-    caseName: "EmitTypespecProject-ServerStub-Js-RightClick",
-    selectType: "Server Stub",
-    selectTypeLanguage: "JavaScript",
-    triggerType: EmitProjectTriggerType.Click,
-    expectedResults: ["http-server-js"],
-  },
 ]
 
 beforeEach(() => {
@@ -126,7 +77,7 @@ describe.each(EmitCasesConfigList)("EmitTypespecProject", async (item) => {
     const { page, app, extensionDir } = await launch({
       workspacePath,
     });
-
+    await sleep(3);
 //    await installExtensionForCommand(page, extensionDir);
     if (triggerType === "Command") {
       await startWithCommandPalette(page, "Emit from Typespec");
